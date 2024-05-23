c1d9032b1e258734414082147ab6516dfbbb97b0
shun-naganuma <129668177+shun-naganuma@users.noreply.github.com>
2024-05-23 09:56:00 +0900

    基本編完了

--- project/helloworldproject/helloworldproject/urls.py
+++ project/helloworldproject/helloworldproject/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#from .views import helloworldfunc, HelloWorldClass
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('helloworldurl/', helloworldfunc),
+    #path('helloworldurl2/', HelloWorldClass.as_view()),
+    path('', include('helloworldapp.urls')),
 ]
+
